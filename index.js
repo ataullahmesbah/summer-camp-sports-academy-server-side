2ab04a0b5763f0d8340b68db23ccbb7d7c0d6bee
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-06-12 02:32:43 +0600

    users patch because user admin role

--- index.js
+++ index.js
@@ -81,6 +81,19 @@ async function run() {
             res.send(result)
         })
 
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+
 
 
 

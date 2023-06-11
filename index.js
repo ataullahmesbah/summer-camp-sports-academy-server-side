dbefd98c45f4058f9f3529d5d65d8c90e9ea1365
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-06-11 06:23:24 +0600

    mongodb users find and toarray

--- index.js
+++ index.js
@@ -61,8 +61,18 @@ async function run() {
         })
 
         // users relevant api
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
             const result = await usersCollection.insertOne(user);
             res.send(result)
         })
